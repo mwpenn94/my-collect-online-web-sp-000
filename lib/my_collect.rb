1c8fd80a3c435f28143d1abc6d889c26702203d5
Michael Penn <mwpenn94@gmail.com>
2020-12-26 17:55:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,5 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
 collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
+def my_collect(collection) do |lang|
   lang.upcase
 end
  
